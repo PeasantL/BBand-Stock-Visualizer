42a18f51b6e08481e24f8ca5b0c6c2165533a379
Samuel Tan <45391854+PeasantL@users.noreply.github.com>
2024-08-25 22:30:03 +0800

    Ammended shell script

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ if [ -d "$VENV_DIR" ]; then
     source $VENV_DIR/bin/activate
 else
     echo "Creating the virtual environment..."
-    python -m venv $VENV_DIR
+    python3 -m venv $VENV_DIR
     echo "Activating the virtual environment..."
     source $VENV_DIR/bin/activate
     echo "Installing requirements..."
@@ -16,7 +16,7 @@ fi
 
 # Run main.py
 echo "Running main.py..."
-python main.py
+python3 main.py
 
 # Deactivate the virtual environment
 deactivate
